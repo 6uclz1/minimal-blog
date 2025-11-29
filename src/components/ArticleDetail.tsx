2915a2c77861eac36b51a09d41ab4b0dd9495802
6uclz1 <9139177+6uclz1@users.noreply.github.com>
2025-11-29 15:51:00 +0900

    feat: remove author display from article detail and list components

--- src/components/ArticleDetail.tsx
+++ src/components/ArticleDetail.tsx
@@ -23,13 +23,6 @@ export const ArticleDetail = (props: { article: Article }) => {
               </svg>
               {formattedDate}
             </time>
-            <span class="text-gray-600">•</span>
-            <div class="flex items-center gap-2">
-              <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-              </svg>
-              {article.author}
-            </div>
           </div>
         </header>
 
