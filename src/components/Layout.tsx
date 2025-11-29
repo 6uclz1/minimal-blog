5ab4e42b8d319343de30dfefc98129d74dca47a3
6uclz1 <9139177+6uclz1@users.noreply.github.com>
2025-11-29 15:21:19 +0900

    Remove Google Fonts (Inter) links from Layout component.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,16 +9,6 @@ export const Layout = (props: {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{props.title}</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossorigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
         <link
           rel="stylesheet"
           href={props.stylePath || "./static/styles.css"}
