c75e4b141c5ca1fe3d446ce3a47513130287d96f
6uclz1 <9139177+6uclz1@users.noreply.github.com>
2026-05-01 17:24:40 +0900

    💣

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => (
-  <footer class="bg-black border-t border-gray-800 mt-auto">
-    <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      <p class="text-center text-gray-400 text-sm">
-        © {new Date().getFullYear()} 6uclz1 Blog. All rights reserved.
-      </p>
-    </div>
-  </footer>
-);
